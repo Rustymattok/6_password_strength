64d6108af0485c6ffe4350a2151b043caa5f47d0
Rustymattok <you@example.com>
2019-11-20 02:14:27 +0300

    обновил согласно всем комментариям от 19.11.2019

--- password_strength.py
+++ password_strength.py
@@ -1,46 +1,46 @@
 import re
 import argparse
 import os
+import getpass
 
 
-def get_pattern_regular(regular_exp):
-    lc = re.compile(regular_exp)
-    return lc
+PATTERN_UP_LETTER = '[A-Z]+'
+PATTERN_NUMERIC = '[0-9]'
+PATTERN_SYMBOLS = r'\W'
 
 
 def get_score_regular(regular_exp, password):
-    score_password = 0
-    size_coincidences = len(get_pattern_regular(regular_exp).findall(password))
+    size_coincidences = len(re.findall(regular_exp, password))
     if 0 < size_coincidences < len(password):
-        score_password = 2.5
-    return score_password
+        return True
+    else:
+        return False
 
 
 def load_data(file_path):
     if not os.path.exists(file_path):
         return None
     with open(file_path, 'r') as file_handler:
-        data_list = file_handler.read().split(' ')
+        data_list = file_handler.read().split('\n')
     return data_list
 
 
 def get_score_black_list(black_list, password):
-    score_password = 0
     password = password.lower()
     for word in black_list:
-        if password.find(word.lower()) != -1:
-            score_password = 0
-            break
+        if word.lower() in password:
+            return False
         else:
-            score_password = 2.5
-    return score_password
+            return True
 
 
 def get_password_strength(black_list, password):
-    common_score = get_score_regular('[A-Z]+', password) + \
-                   get_score_regular('[0-9]', password) + \
-                   get_score_regular('\\W', password) + \
-                   get_score_black_list(black_list, password)
+    common_score = sum(
+        [get_score_regular(PATTERN_UP_LETTER, password) * 2.5,
+         get_score_regular(PATTERN_NUMERIC, password) * 2.5,
+         get_score_regular(PATTERN_SYMBOLS, password) * 2.5,
+         get_score_black_list(black_list, password) * 2.5]
+    )
     return common_score
 
 
@@ -49,11 +49,9 @@ def create_parser():
     parser.add_argument(
         '-f',
         '--file',
-        required=True,
+        default='Most-Popular-Letter-Passes.txt',
         help='command - input file'
     )
-    parser.add_argument('-p', '-password', required=True,
-                        help='command - password which should check')
     return parser
 
 
@@ -62,11 +60,12 @@ def main():
     args = parser.parse_args()
     try:
         black_list = load_data(args.file)
-        password = args.p
-        print(
-            'score of password: ',
-            get_password_strength(black_list, password)
-        )
+        password = getpass.getpass()
+        print('score password: ' +
+              password +
+              ' : ' +
+              str(get_password_strength(black_list, password))
+              )
     except ValueError:
         print('not correct format')
 
